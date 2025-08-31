104cd5717cf92c372e213f4ddde838c75c39ee02
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-08-31 17:36:37 +0530

    practice

--- 01-shell.sh
+++ 01-shell.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "ramesh: Hi suresh, how are you?"
-echo "suresh: Hello ramesh, im fine thank you"
-echo "ramesh: what are you doing for living?"
-echo "suresh: im working for google"
+Person1=ramesh
+Person2=suresh
+
+echo "$Person1: Hi $Person2, how are you?"
+echo "${Person2}: Hello ${Person1}, im fine thank you"
+echo "${Person1}: what are you doing for living?"
+echo "${Person2}: im working for google"
