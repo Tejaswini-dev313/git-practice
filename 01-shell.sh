bacca22abec6f8cf5e098c75c61c1329f522e2c5
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-08-31 17:54:15 +0530

    practice

--- 01-shell.sh
+++ 01-shell.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-FRUIT=("$a" "$b" "$c")
+FRUIT=("apple" "mango" "kiwi")
 
-echo "First name of the fruit is ${FRUIT[$a]}"
+echo "First name of the fruit is ${FRUIT[0]}"
 echo "Second name of the fruit is ${FRUIT[1]}"
 echo "Third name of the fruit is ${FRUIT[2]}"
+
+echo "Total names of the fruits are ${FRUIT[@]}"
