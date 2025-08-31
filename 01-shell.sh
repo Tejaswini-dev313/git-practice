8e550647a59d92fa0cb6b452b702737ee9a1820f
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-08-31 17:49:56 +0530

    practice

--- 01-shell.sh
+++ 01-shell.sh
@@ -2,6 +2,6 @@
 
 FRUIT=("$a" "$b" "$c")
 
-echo "First name of the fruit is ${FRUIT[0]}"
+echo "First name of the fruit is ${FRUIT[$a]}"
 echo "Second name of the fruit is ${FRUIT[1]}"
 echo "Third name of the fruit is ${FRUIT[2]}"
