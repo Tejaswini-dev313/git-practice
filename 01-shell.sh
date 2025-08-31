a141c18dc730ed605eecade7720274e7c21d8947
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-08-31 17:47:30 +0530

    practice

--- 01-shell.sh
+++ 01-shell.sh
@@ -2,6 +2,6 @@
 
 FRUIT=("apple" "kiwi" "orange")
 
-echo "First name of the fruit ${FRUIT[0]}"
-echo "Second name of thevfruit ${FRUIT[1]}"
+echo "First name of the fruit is ${FRUIT[0]}"
+echo "Second name of the fruit is ${FRUIT[1]}"
 echo "Third name of the fruit is ${FRUIT[2]}"
