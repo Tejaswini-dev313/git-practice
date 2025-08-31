fcdf6ddbb1475f69c221c57b14584e9e607f0c0b
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-08-31 18:13:16 +0530

    practice

--- 02-command.sh
+++ 02-command.sh
@@ -3,6 +3,6 @@
 Person1=$1
 Person2=$2
 
-SUM=$(($Person1+$Person2))
+SUM=$(($Person1-$Person2))
 
-echo "sum of $Person1 and $Person2 is equal to $SUM"
+echo "substraction of $Person1 and $Person2 is equal to $SUM"
