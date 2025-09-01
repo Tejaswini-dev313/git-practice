8ef5e3e017a1a99ae1c60162776bc4979ca19528
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-09-01 12:22:57 +0530

    shell

--- 02-command.sh
+++ 02-command.sh
@@ -2,29 +2,26 @@
 
 USERID=$(id -u)
 
-echo "user id is: $USERID"
+echo "User ID is: $USERID"
 
 if [ $USERID -ne 0 ]
-then 
-    echo "please run the script with root privileages"
+then
+    echo "Please run this script with root priveleges"
     exit 1
 fi
 
-#install mysql
-
-dnf list insatlled mysql
+dnf list installed mysql
 
 if [ $? -ne 0 ]
 then 
-    echo "mysql server is not installed. please install"
+    echo "mysql installation is not success and check it"
     dnf install mysql -y
     if [ $? -ne 0 ]
     then
-        echo "installed mysql not success"
+        echo "mysql installation is not success"
         exit 1
     else
-        echo "installed successfully"
+        echo "mysql installation is success"
     fi
 else
-    echo "mysql server already installed. nothing to do"
-fi
+    echo "mysql is already installed"
