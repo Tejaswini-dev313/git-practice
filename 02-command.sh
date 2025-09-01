06f92d02dfad2580d183e26078afcb80d542468b
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-09-01 12:36:11 +0530

    shell

--- 02-command.sh
+++ 02-command.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-dnf install mysql -y
+USERID=$(id -u)
+
+echo "user id is: $USERID"
+
+if [ $USERID -ne 0 ]
+then 
+    echo "please run the script with root privileages"
+    exit 1
+fi
+
+#install mysql
+
+dnf list insatlled mysql
+
+if [ $? -ne 0 ]
+then 
+    echo "mysql server is not installed. please install"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "installed mysql not success"
+        exit 1
+    else
+        echo "installed successfully"
+    fi
+else
+    echo "mysql server already installed. nothing to do"
+fi
