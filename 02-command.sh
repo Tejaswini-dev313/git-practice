d12764fb98e6cb658e8aa8d0accee237c7253a04
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-09-04 17:43:13 +0530

    practice

--- 02-command.sh
+++ 02-command.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-USERID=$(id -u)
-
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
+USERID={id -u}
 
 echo "user id is $USERID"
 
 if [ $USERID -ne 0 ]
-then
+then 
     echo "run the scrip with root priveleges"
     exit 1
 fi
@@ -17,30 +13,19 @@ fi
 VALIDATE (){
     if [ $1 -ne 0 ]
     then 
-        echo -e "$2 is $R failed $N"
-    else
-        echo -e "$2 is $G success $N"
+        echo "$2 is failed"
+    else 
+        echo "$2 is success"
     fi
 }
 
-dnf list installed mysql
+dnf installed list 
 
 if [ $? -ne 0 ]
-then 
-    echo "mysql is not installed. install it"
+then
+    echo "mysql is not installed. please install it"
     dnf install mysql -y
     VALIDATE $? "installing mysql"
 else
-    echo "mysql is alredy installed. nothing to do"
+    echo "musql has been already installed. nothing to do"
 fi
-
-# dnf list installed git
-
-# if [ $? -ne 0 ]
-# then 
-#     echo "git is not installed. install it"
-#     dnf install git -y
-#     VALIDATE $? "installing git"
-# else
-#     echo "git is alredy installed. nothing to do"
-# fi
