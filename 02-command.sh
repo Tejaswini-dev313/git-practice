aabe428aa8cd34b9819eaa159f6aa798043786c6
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-09-05 19:51:50 +0530

    practice

--- 02-command.sh
+++ 02-command.sh
@@ -5,36 +5,39 @@ do
     echo $i
 done
 
-# USERID=$(id -u)
+USERID=$(id -u)
 
-# R="\e[31m"
-# G="\e[32m"
-# N="\e[0m"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
-# echo "user id is $USERID"
+echo "user id is $USERID"
 
-# if [ $USERID -ne 0 ]
-# then 
-#     echo "run the scrip with root priveleges"
-#     exit 1
-# fi
+if [ $USERID -ne 0 ]
+then 
+    echo "run the scrip with root priveleges"
+    exit 1
+fi
 
-# VALIDATE (){
-#     if [ $1 -ne 0 ]
-#     then 
-#         echo -e "$2 is $R failed $N"
-#     else 
-#         echo -e "$2 is $G success $N"
-#     fi
-# }
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 is $R failed $N"
+    else 
+        echo -e "$2 is $G success $N"
+    fi
+}
 
-# dnf list installed mysql
+for package in $@
+do 
+    dnf list installed $package
 
-# if [ $? -ne 0 ]
-# then
-#     echo "mysql is not installed. please install it"
-#     dnf install mysql -y
-#     VALIDATE $? "installing mysql"
-# else
-#     echo "musql has been already installed. nothing to do"
-# fi
+    if [ $? -ne 0 ]
+    then
+        echo "mysql is not installed. please install it"
+        dnf install $package -y
+        VALIDATE $? "installing $package"
+    else
+        echo "$package has been already installed. nothing to do"
+    fi
+done 
