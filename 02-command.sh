3e620a3c8edd6d54b0592d3e8566c929d7db625d
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-09-05 19:43:38 +0530

    practice

--- 02-command.sh
+++ 02-command.sh
@@ -1,35 +1,40 @@
-#!/bin/bash
+# #!/bin/bash
 
-USERID=$(id -u)
+for i in {0...20}
+do 
+    echo $i
+done
 
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
+# USERID=$(id -u)
 
-echo "user id is $USERID"
+# R="\e[31m"
+# G="\e[32m"
+# N="\e[0m"
 
-if [ $USERID -ne 0 ]
-then 
-    echo "run the scrip with root priveleges"
-    exit 1
-fi
+# echo "user id is $USERID"
 
-VALIDATE (){
-    if [ $1 -ne 0 ]
-    then 
-        echo -e "$2 is $R failed $N"
-    else 
-        echo -e "$2 is $G success $N"
-    fi
-}
+# if [ $USERID -ne 0 ]
+# then 
+#     echo "run the scrip with root priveleges"
+#     exit 1
+# fi
 
-dnf list installed mysql
+# VALIDATE (){
+#     if [ $1 -ne 0 ]
+#     then 
+#         echo -e "$2 is $R failed $N"
+#     else 
+#         echo -e "$2 is $G success $N"
+#     fi
+# }
 
-if [ $? -ne 0 ]
-then
-    echo "mysql is not installed. please install it"
-    dnf install mysql -y
-    VALIDATE $? "installing mysql"
-else
-    echo "musql has been already installed. nothing to do"
-fi
+# dnf list installed mysql
+
+# if [ $? -ne 0 ]
+# then
+#     echo "mysql is not installed. please install it"
+#     dnf install mysql -y
+#     VALIDATE $? "installing mysql"
+# else
+#     echo "musql has been already installed. nothing to do"
+# fi
