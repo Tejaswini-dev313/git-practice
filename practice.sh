cc96496fa7f6ccc0797c3e73a3934fa18a400c3f
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-09-06 19:18:27 +0530

    expense_project

--- practice.sh
+++ practice.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+failure(){
+    
+    echo "failed at $1:$2"
+}
+
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR
+
 echo "Hello, running the script"
 echooo "Hii, learning script"
 echo "I have to get 3 L/m salary and have to learn more"
