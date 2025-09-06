48774891a13089f6bff3358986e505758e483ee8
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-09-06 12:49:36 +0530

    practice

--- 03-backend.sh
+++ 03-backend.sh
@@ -27,13 +27,13 @@ VALIDATE (){
     fi
 }
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y | tee -a $LOG_FILE
 VALIDATE $? "Disabling nodejs"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y | tee -a $LOG_FILE
 VALIDATE $? "Enableing nodejs"
 
-dnf install nodejs -y
+dnf install nodejs -y | tee -a $LOG_FILE
 VALIDATE $? "installing nodejs"
 
 id expense
@@ -41,13 +41,13 @@ id expense
 if [ $? -ne 0 ]
 then
     echo "Expense user not created. please create user"
-    useradd expense
+    useradd expense | tee -a $LOG_FILE
     VALIDATE $? "creating expense user"
 else
     echo "Expense user already created. nothing to do.."
 fi
 
-mkdir -p /app
+mkdir -p /app | tee -a $LOG_FILE
 VALIDATE $? "Creating app"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip | tee -a $LOG_FILE
@@ -61,6 +61,19 @@ VALIDATE $? "Extracting backend application code"
 npm install &>>$LOG_FILE
 cp /home/ec2-user/git-practice/backend.service /etc/systemd/system/backend.service
 
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Client"
 
+mysql -h mysql.daws81s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+VALIDATE $? "Schema loading"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Daemon reload"
+
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "Enabled backend"
+
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "Restarted Backend"
 
 
