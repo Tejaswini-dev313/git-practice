65ab8effec3391cdac73f54f313311df1b8e55ea
Tejaswini-dev313 <bodapatlatejaswini1@gamil.com>
2025-09-06 12:47:41 +0530

    practice

---
+++ 03-backend.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+LOG_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOG_FOLDER
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then 
+    echo "run the scrip with root priveleges" | tee -a $LOG_FILE
+    exit 1
+fi
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 is $R failed $N" | tee -a $LOG_FILE
+    else 
+        echo -e "$2 is $G success $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf module disable nodejs -y
+VALIDATE $? "Disabling nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enableing nodejs"
+
+dnf install nodejs -y
+VALIDATE $? "installing nodejs"
+
+id expense
+
+if [ $? -ne 0 ]
+then
+    echo "Expense user not created. please create user"
+    useradd expense
+    VALIDATE $? "creating expense user"
+else
+    echo "Expense user already created. nothing to do.."
+fi
+
+mkdir -p /app
+VALIDATE $? "Creating app"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip | tee -a $LOG_FILE
+VALIDATE $? "Downloading backend application code"
+
+cd /app
+rm -rf /app/* # remove the existing code
+unzip /tmp/backend.zip &>>$LOG_FILE
+VALIDATE $? "Extracting backend application code"
+
+npm install &>>$LOG_FILE
+cp /home/ec2-user/git-practice/backend.service /etc/systemd/system/backend.service
+
+
+
+
